3c06e422e82be950092a4f6d64b5d5692ed2e088
Karla-Mihai <2880904M@student.gla.ac.uk>
2025-02-03 12:38:08 +0000

    End of chapter 4

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context_dict)
+
+def about(request):
+    context_dict = {
+        'message': "This tutorial has been put together by Karla Mihai",
+        'MEDIA_URL': settings.MEDIA_URL  # Make sure MEDIA_URL is passed into the context
+    }
+    return render(request, 'rango/about.html', context_dict)
+
+
